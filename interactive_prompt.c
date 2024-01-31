854523a4d6a39519283f601b84316b7833be5836
EvanA <amedievan@icloud.com>
2024-01-31 17:49:50 -0500

    windows compatible

--- interactive_prompt.c
+++ interactive_prompt.c
@@ -1,8 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include <editline/readline.h>
+// if we are compiling on Windows compile these functions
+#ifdef _WIN32
+#include <string.h>
 
+static char buffer[2048];
+
+// fake readline function
+char* readline(char* prompt) {
+	fputs(prompt, stdout);
+	fgets(buffer, 2048, stdin);
+	char* cpy = malloc(strlen(buffer) + 1);
+	strcpy(cpy, buffer);
+	cpy[strlen(cpy) - 1] = '\0';
+	return cpy;
+}
+
+// fake add_history function
+void add_history(char* unused) {}
+
+// otherwise include the editline headers
+#else
+#include <editline/readline.h>
+// #include <editline/history.h>
+#endif
 
 int main(int argc, char** argv) {
 
@@ -20,7 +42,7 @@ int main(int argc, char** argv) {
 		add_history(input);
 		
 		// echo input back to user
-		printf("No you're a %s\n", input);
+		printf("hmmmm, so what you're saying is %s?\n", input);
 		
 		// free retrieved input
 		free(input);
