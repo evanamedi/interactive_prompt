70c430abc6c8d873eab90912a7071d81722e88a4
EvanA <amedievan@icloud.com>
2024-01-31 13:28:29 -0500

    add prompt nav

--- interactive_prompt.c
+++ interactive_prompt.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-// declare a buffer for user input of size 2040
-static char input[2048];
+#include <editline/readline.h>
+
 
 int main(int argc, char** argv) {
 
@@ -12,14 +13,17 @@ int main(int argc, char** argv) {
 	// in a never ending loop
 	while (1) {
 	
-		// output prompt
-		fputs("ADP> ", stdout);
+		// output our prompt and get input
+		char* input = readline("ADP> ");
 		
-		// read a line of user input of maximun size 2048
-		fgets(input, 2048, stdin);
+		// add input to history
+		add_history(input);
 		
 		// echo input back to user
-		printf("No you're a %s", input);
+		printf("No you're a %s\n", input);
+		
+		// free retrieved input
+		free(input);
 	}
 	
 	return 0;
