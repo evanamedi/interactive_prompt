2968eb103681a53203fbc8cce97545ea468fd55f
EvanA <amedievan@icloud.com>
2024-01-31 21:16:45 -0500

    style

--- learn_mpc_parser.c
+++ learn_mpc_parser.c
@@ -28,8 +28,8 @@ mpc_parser_t* Doge = mpc_many(mpcf_strfold, Phrase);
 // "so c"
 
 //
-// recreate previous examples using new method:
 //
+// recreate previous examples using new method:
 
 mpc_parser_t* Adjective = mpc_new("adjective");
 mpc_parser_t* Noun = mpc_new("noun");
@@ -42,7 +42,32 @@ mpca_lang(MPCA_LANG_DEFAULT,
 		noun	  : \"lisp\" | \"language\" | \"book\" | \"build\" | \"c\";	\
 		phrase	  : <adjective> <noun>;										\
 		doge	  : <phrase>*;												\
-		",
+	",
 		Adjective, Noun, Phrase, Doge);
 // do some parsing here...
 mpc_cleanup(4, Adjective, Noun, Phrase, Doge);
+
+
+// special symbol rules on right hand side work as follows:
+// "ab"			The string ab is required
+// 'a'			The character a is required
+// 'a' 'b'		First 'a' is required, then 'b' is required
+// 'a' | 'b'	Either 'a' is required, or 'b' is required
+// 'a'*			Zero or more 'a' are required
+// 'a'+			One or more 'a' are required
+// <abba>		The rule called abba is required
+
+
+// some things to think about:
+// why are there back slashes <\> in front of the quote marks <"> in the grammar?
+// why are there back slashes <\> at the end of the line in the grammer
+
+
+// some things to do:
+// write down some more examples of string the <Doge> language contains
+// descrive textually a grammer for decimal numbers such as 0.01 or 52.221
+// describe textually a grammer for web URLs such as <https://www.buildyourownlisp.com>
+// describe textually a grammer for simple english sentences such as:
+//					"the cat sat on the mat"
+// describe more formally the above grammers. use <|>, <*>, or any symbols of own invention
+// textually describe a grammer for JSON
